38a741b61cf5da3fe395c0a902c843be09165d03
纳兰天忆 <103520146@qq.com>
2016-12-16 15:58:25 +0800

    webpack 生产环境配置

--- app/index.js
+++ app/index.js
@@ -6,12 +6,12 @@ import generateText from './sub';
 import  $ from 'jquery';
 import 'imports?jQuery=jquery!./plugin';
 import  moment from 'moment';
-let app=document.createElement('div');
-const myPromise=Promise.resolve(42);
-myPromise.then((number)=>{
-    $('body').append('<p>promise result is'+number+' now is '+moment().format('YYYY-MM-DD HH:mm:ss')+'</p>');
+let app = document.createElement('div');
+const myPromise = Promise.resolve(42);
+myPromise.then((number) => {
+    $('body').append('<p>promise result is' + number + ' now is ' + moment().format('YYYY-MM-DD HH:mm:ss') + '</p>');
     $('p').greenify();
 });
-app.innerHTML='<h1>Hello World it</h1>';
+app.innerHTML = '<h1>Hello World it</h1>';
 document.body.appendChild(app);
 app.appendChild(generateText());

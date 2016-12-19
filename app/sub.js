8c1f9c9d06979712b9b75097cfb3012dad152887
纳兰天忆 <103520146@qq.com>
2016-12-19 11:31:32 +0800

    webpack react init

--- app/sub.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by nalantianyi on 2016/12/13.
- */
-export default function() {
-    var element = document.createElement('h2');
-    element.innerHTML = 'Hello h2 world';
-    return element;
-}
